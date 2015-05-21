5e46f19780c6da359b404a2caf739c74f7fe447b
anbinh <tinhoc@outlook.com>
2015-05-20 22:02:54 -0400

    first commit

---
+++ public/app/websites/js/app.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $(".login-btn").click(function(){
+        $("#modal-login").modal("show");
+        // Ideally show the person from SSL and prepopulate
+    });
+
+    function centerModal() {
+        $(this).css("display", "block");
+        var $dialog = $(this).find(".modal-dialog");
+        var offset = ($(window).height() - $dialog.height()) / 4;
+        // Center modal vertically in window
+        $dialog.css("margin-top", offset);
+    }
+
+    $(".modal").on("show.bs.modal", centerModal);
+    $(".home").each(function(){
+        $(this).click(function(){
+            window.location.href = "./index.html";
+        });
+    });
+    $("#editProfile").click(function(){
+        window.location.href = "./profileEdit.html";
+    });
+    $("#editProfileDone").click(function(){
+        window.location.href = "./profile.html";
+    });
+    $(".post-btn").each(function(){
+        $(this).click(function(){
+            window.location.href = "./postproject.html";
+        });
+    });
+    $(".btn-default").each(function(){
+        $(this).click(function(){
+            window.location.href = "./projectdetails.html";
+        });
+    });
+
+    $(window).on("resize", function () {
+        $(".modal:visible").each(centerModal);
+    });
+});
