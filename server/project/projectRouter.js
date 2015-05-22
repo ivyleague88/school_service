336b3cf1ac4ed36a404e1b4a64633d21cf14e4c6
anbinh <tinhoc@outlook.com>
2015-05-21 23:49:55 -0400

    fix bugs

--- server/project/projectRouter.js
+++ server/project/projectRouter.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
-var Project = require(__dirname + '/ProjectSchema');
+var Project = require('./projectSchema');
 
 /* GET /projects listing. */
 router.get('/', function(req, res, next) {
